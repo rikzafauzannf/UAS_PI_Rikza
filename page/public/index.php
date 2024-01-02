e35f12d75be2cc6ffcd35c0c17617c72b0187712
rikzafauzannf <rikzarazor491@gmail.com>
2024-01-02 18:02:05 +0700

    user data mobil

--- page/public/index.php
+++ page/public/index.php
@@ -2,24 +2,30 @@
     <div class="container-fluid">
         <h2 class="fw-bold">-Mobil Ready</h2>
         <div class="row">
-            <div class="col-md-4">
-                <div class="card border border-0 shadow">
-                    <div class="card-body">
-                        <div class="row">
-                            <?php
-
-                            ?>
-                            <div class="col-md-12">
-                                <h3>Mobil</h3>
-                                <hr>
+            <?php
+            include("config/connect.php");
+            $queryMobil = mysqli_query($link, "SELECT * FROM tbl_mobil_rikza");
+            while ($row = mysqli_fetch_assoc($queryMobil)) :
+            ?>
+                <div class="col-md-4">
+                    <div class="card border border-0 shadow">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3><?= $row['nama_mobil_rikza'] ?></h3>
+                                    <hr>
+                                </div>
+                                <div class="col-md-4 fs-5"><?= $row['no_plat_rikza'] ?></div>
+                                <div class="col-md-4 fs-5"><?= $row['brand_mobil_rikza'] ?></div>
+                                <div class="col-md-4 fs-5"><?= $row['tipe_transmisi_rikza'] ?></div>
+                                <div class="col-md-12 mt-3">
+                                    <a href="" class="btn btn-md btn-primary w-100">Rental</a>
+                                </div>
                             </div>
-                            <div class="col-md-4 fs-5">B 0939 ls</div>
-                            <div class="col-md-4 fs-5">mazda</div>
-                            <div class="col-md-4 fs-5">Manual</div>
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php endwhile; ?>
         </div>
     </div>
 </main>
